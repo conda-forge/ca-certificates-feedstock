39906355804f7f3352923d99ba9f80bf1f5d393c
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-07-07 14:11:47 -0400

    recipe: Handle `CONDA_PREFIX` in some cases on Windows.

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,2 +1,3 @@
-set "SSL_CERT_DIR=%CONDA_ENV_PATH%\ssl"
+:: handle conda...   >=4.1.3        <= 4.1.2
+set "SSL_CERT_DIR=%CONDA_PREFIX%%CONDA_ENV_PATH%\ssl"
 set "SSL_CERT_FILE=%SSL_CERT_DIR%\cacert.pem"
