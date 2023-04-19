fb49c4430736e14ff7f547c581f5f4d55d17514e
Komarova, Evseniia <evseniia.komarova@intel.com>
2023-04-19 16:31:24 +0200

    upd build scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,6 @@
 # Create the directory to hold the certificates.
 mkdir -p "${PREFIX}/ssl"
 
-# Copy the certificates from certifi.
-cp -f "${SRC_DIR}/certifi/cacert.pem" "${PREFIX}/ssl"
+# Move the certificates.
+mv cacert.pem ${PREFIX}/ssl/cacert.pem
 ln -fs "${PREFIX}/ssl/cacert.pem" "${PREFIX}/ssl/cert.pem"
